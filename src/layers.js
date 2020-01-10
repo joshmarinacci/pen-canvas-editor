16ba54518f113abe652a26ca25a50f47d47b2f77
josh marinacci <joshua@marinacci.org>
2020-01-09 16:30:08 -0800

    lazily create tiles

--- src/layers.js
+++ src/layers.js
@@ -10,7 +10,9 @@ class Tile {
     constructor(size) {
         this.size = size
         this.canvas = null
-        this.initCanvas()
+    }
+    empty() {
+        return this.canvas === null
     }
     clear() {
         if(this.canvas) {
@@ -63,9 +65,7 @@ export class Layer {
     }
     getFilledTileCount() {
         let total = 0
-        this.forAllTiles(t => {
-            if(t.canvas) total++
-        })
+        this.forAllTiles(t => total += t.empty()?0:1)
         return total
     }
 
@@ -87,27 +87,45 @@ export class Layer {
     }
 
     stamp(brush,x,y,flow,opacity,blend){
-        //draws onto every tile
-        this.forAllTiles((t,i,j)=>{
-            const ctx = t.getCanvas().getContext('2d')
-            ctx.save()
-            ctx.globalAlpha = flow
-            ctx.globalCompositeOperation = blend
-            ctx.drawImage(brush,x-i*TILE_SIZE,y-j*TILE_SIZE,brush.width,brush.height)
-            ctx.restore()
-        })
+        //calculate the minimum tiles needed for the brush
+        let sx = Math.floor(x/TILE_SIZE)
+        let sy = Math.floor(y/TILE_SIZE)
+        let ex = Math.floor((x+brush.width)/TILE_SIZE)+1
+        let ey = Math.floor((y+brush.height)/TILE_SIZE)+1
+        // console.log(`drawing ${sx}-${ex} ${sy}-${ey}  for ${x},${y}`)
+        ex = Math.min(ex,this.tiles.length)
+        ey = Math.min(ey,this.tiles[0].length)
+        //stamp onto the selected tiles
+        for(let i=sx; i<ex; i++) {
+            for(let j=sy;j<ey;j++) {
+                let t = this.tiles[i][j]
+                const ctx = t.getCanvas().getContext('2d')
+                ctx.save()
+                ctx.globalAlpha = flow
+                ctx.globalCompositeOperation = blend
+                ctx.drawImage(brush,x-i*TILE_SIZE,y-j*TILE_SIZE,brush.width,brush.height)
+                ctx.restore()
+            }
+        }
     }
     clear() {
         this.forAllTiles(t => t.clear())
     }
-    drawSelf(ctx) {
+    drawSelf(c) {
         this.forAllTiles((t,i,j) => {
-            ctx.drawImage(t.getCanvas(),i*TILE_SIZE,j*TILE_SIZE)
+            if(t.empty()) return
+            c.save()
+            c.translate(i*TILE_SIZE, j*TILE_SIZE)
+            c.drawImage(t.getCanvas(),0,0)
+            c.fillStyle = 'green'
+            c.strokeStyle = 'green'
+            c.strokeRect(5,5,t.size-5,t.size-5)
+            c.restore()
         })
     }
     drawLayer(layer, opacity=1.0, blend='src-atop') {
-        // console.log('drawing layer',layer.title,'to',this.title)
         layer.forAllTiles((srcTile,i,j) => {
+            if(srcTile.empty()) return
             const dstTile = this.tiles[i][j]
             const c = dstTile.getCanvas().getContext('2d')
             c.save()
